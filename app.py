8ec04561522a7fc7352c7d90b5cfa32260b95c04
VardhiniT <134366869+VardhiniT@users.noreply.github.com>
2024-01-27 17:39:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,14 +2,13 @@ from flask import Flask,request,render_template
 import numpy as np
 import pandas as pd
 
+
 from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import CustomData,PredictPipeline
 
 app=Flask(__name__)
 
-
 ## Route for a home page
-
 @app.route('/')
 def index():
     return render_template('index.html') 
